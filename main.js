522098b6f445154b415d58b79dd3224e78e3635f
disham910 <75997822+disham910@users.noreply.github.com>
2021-03-31 19:36:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,33 @@ Webcam.set({
     image_format:"png",
     png_quality:90
 })
-webcam.attach("#camera");
-function take_snapshot(){
-    Webcam_snap(function(data){
+Webcam.attach("#camera");
+function snapshot(){
+    Webcam.snap(function(data){
         document.getElementById("result").innerHTML="<img id='captuered_image' src='"+data+"'>";
 
     })
 }
-console.log ("ml5.version",ml5.version);
-classifier=ml5.imageclassifier('https://teachablemachine.withgoogle.com/models/xlF-Fr_8S/model.json',modelloaded);
-)
+console.log ("ml5 version",ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xlF-Fr_8S/model.json',modelloaded);
+function modelloaded(){
+    console.log("model loaded");
+
+}
+function check(){
+    img=document.getElementById ("captuered_image");
+    classifier.classify(img,gotresult);
+}
+function gotresult(error,result){
+    if(error){
+        console.log(error);
+
+    }
+else{
+    console.log(result);
+    document.getElementById("result_object_name").innerHTML=result[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=result[0].confidence.toFixed(3);
+
+
+}
+}
